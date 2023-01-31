595fc6ec255ef3351d474172b31663adb6a9ba50
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-01-31 11:38:33 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,29 @@
+/* Перше */
 
+const USER_NUMBER_ONE = Number(prompt('Введіть будь яке перше число діапазону!'));
+const USER_NUMBER_TWO = Number(prompt('Введіть будь яке друге число діапазону!'));
+let userNumber = USER_NUMBER_ONE + USER_NUMBER_TWO;
+console.log(userNumber)
+
+
+
+// function range(x,y){
+//   var listRangeNumber = [];
+//   while( x <= y ){
+//       listRangeNumber.push(x);
+//       x++;
+//   }
+//   return listRangeNumber;
+// }
+
+// function sum(range){
+//   var listSumNumber = 0;
+//   for (var k in range) {
+//       listSumNumber += range[k];
+//   }
+//   return listSumNumber;
+// }
+
+// alert( "Range this numbers " + range(x,y));
+// alert (sum(range(x,y)));
 
